7d820599a7089868a936f41aea0af6d8cb68cad9
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 06:11:35 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -53,7 +53,7 @@ selective_tweet_shortener(tweet_three)
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140 
-    word_substituter(tweet)[0..139] + '...'
+    word_substituter(tweet)[0..137] + '...'
   else 
     tweet 
   end 
